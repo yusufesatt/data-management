3fd949875f6d4bb016827dff2c13136383c371ed
yusufesatt <xyusufesat@gmail.com>
2023-12-06 15:11:22 +0300

    converted to empty_data_remover.py class structure and added usage with argparse

--- empty_data_remover.py
+++ empty_data_remover.py
@@ -1,59 +1,61 @@
 import os
 from tqdm import tqdm
+import argparse
 
-# Can be converted to class structure in the future
+class DataRemover:
+    def __init__(self, root_folder):
+        self.root_folder = root_folder
 
-def remove_empty_data (labels_folder, images_folder):
-    initial_label_count = len([f for f in os.listdir(labels_folder) if f.endswith('.txt')])
-    initial_image_count = len([f for f in os.listdir(images_folder) if
-                               any(f.endswith(ext) for ext in [".jpg", ".jpeg", ".png", ".bmp", ".gif", '.JPG', '.PNG', '.JPEG'])])
+    def remove_empty_data(self, labels_folder, images_folder):
+        initial_label_count = len([f for f in os.listdir(labels_folder) if f.endswith('.txt')])
+        initial_image_count = len([f for f in os.listdir(images_folder) if
+                                   any(f.endswith(ext) for ext in [".jpg", ".jpeg", ".png", ".bmp", ".gif", '.JPG', '.PNG', '.JPEG'])])
 
-    label_files = [f for f in os.listdir(labels_folder) if f.endswith('.txt')]
+        label_files = [f for f in os.listdir(labels_folder) if f.endswith('.txt')]
 
-    for label_file in label_files:
-        label_file_path = os.path.join(labels_folder, label_file)
-        base_filename, _ = os.path.splitext(label_file)
-        possible_image_extensions = [".jpg", ".jpeg", ".png", ".bmp", ".gif", '.JPG', '.PNG', '.JPEG']
+        for label_file in label_files:
+            label_file_path = os.path.join(labels_folder, label_file)
+            base_filename, _ = os.path.splitext(label_file)
+            possible_image_extensions = [".jpg", ".jpeg", ".png", ".bmp", ".gif", '.JPG', '.PNG', '.JPEG']
 
-        image_file_path = None
-        for ext in possible_image_extensions:
-            possible_image_path = os.path.join(images_folder, f"{base_filename}{ext}")
-            if os.path.exists(possible_image_path):
-                image_file_path = possible_image_path
-                break
+            image_file_path = None
+            for ext in possible_image_extensions:
+                possible_image_path = os.path.join(images_folder, f"{base_filename}{ext}")
+                if os.path.exists(possible_image_path):
+                    image_file_path = possible_image_path
+                    break
 
-        if image_file_path and os.path.exists(label_file_path) and os.path.getsize(label_file_path) == 0:
-            print(f"Deleting {label_file_path} and {image_file_path}")
-            os.remove(label_file_path)
-            os.remove(image_file_path)
+            if image_file_path and os.path.exists(label_file_path) and os.path.getsize(label_file_path) == 0:
+                print(f"Deleting {label_file_path} and {image_file_path}")
+                os.remove(label_file_path)
+                os.remove(image_file_path)
 
-    final_label_count = len([f for f in os.listdir(labels_folder) if f.endswith('.txt')])
-    final_image_count = len([f for f in os.listdir(images_folder) if
-                             any(f.endswith(ext) for ext in [".jpg", ".jpeg", ".png", ".bmp", ".gif", '.JPG', '.PNG', '.JPEG'])])
+        final_label_count = len([f for f in os.listdir(labels_folder) if f.endswith('.txt')])
+        final_image_count = len([f for f in os.listdir(images_folder) if
+                                 any(f.endswith(ext) for ext in [".jpg", ".jpeg", ".png", ".bmp", ".gif", '.JPG', '.PNG', '.JPEG'])])
 
-    print(f"Initial Label Count: {initial_label_count}")
-    print(f"Final Label Count: {final_label_count}")
-    print(f"Initial Image Count: {initial_image_count}")
-    print(f"Final Image Count: {final_image_count}")
+        print(f"Initial Label Count: {initial_label_count}")
+        print(f"Final Label Count: {final_label_count}")
+        print(f"Initial Image Count: {initial_image_count}")
+        print(f"Final Image Count: {final_image_count}")
 
+    def process_folders(self):
+        for folder in os.listdir(self.root_folder):
+            if (not folder.endswith('.yaml')) and (not folder.endswith('.txt')):
+                folder_path = os.path.join(self.root_folder, folder)
+                labels_folder = os.path.join(folder_path, 'labels')
+                images_folder = os.path.join(folder_path, 'images')
 
-def process_folders(root_folder):
-    for folder in os.listdir(root_folder):
-        if (not folder.endswith('.yaml')) and (not folder.endswith('.txt')):
-            folder_path = os.path.join(root_folder, folder)
-            labels_folder = os.path.join(folder_path, 'labels')
-            images_folder = os.path.join(folder_path, 'images')
+                if os.path.exists(labels_folder) and os.path.exists(images_folder):
+                    print(f"Processing folder: {folder_path}")
+                    self.remove_empty_data(labels_folder, images_folder)
+                    print("")
 
-            if os.path.exists(labels_folder) and os.path.exists(images_folder):
-                print(f"Processing folder: {folder_path}")
-                remove_empty_data(labels_folder, images_folder)
-                print("")
+argparse = argparse.ArgumentParser()
+argparse.add_argument("--input_path", type=str, required=True)
+args = argparse.parse_args()
 
-
-def main():
-    root_folder = r"path/to/your/root/folder" # your root folder path
-    process_folders(root_folder)
-
-
-if __name__ == "__main__":
-    main()
+print(f"Root Folder Path: {args.input_path}")
+input_path = args.input_path
+data_remover = DataRemover(input_path)
+data_remover.process_folders()
