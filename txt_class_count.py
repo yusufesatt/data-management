ff0a4caea44090aad58bca4454303a12ae88a735
Yusuf Esat <xyusufesat@gmail.com>
2024-05-04 17:05:27 +0300

    Improvement

--- txt_class_count.py
+++ txt_class_count.py
@@ -1,14 +1,19 @@
+"""
+    This project reads the txt files in the specified folder
+    and calculates how many annotations there are for each class.
+"""
+import argparse
 import os
 from collections import defaultdict
-import argparse
 
 
-def txt_folders_class_count(input_path):
+def txt_folders_class_count(path):
     """
-    This function reads the txt files in the specified folder and calculates how many annotations there are for each class.
+    This function reads the txt files in the specified folder
+    and calculates how many annotations there are for each class.
 
     Args:
-        input_path (str) : Path to the folder where the txt files are located.
+        path (str) : Path to the folder where the txt files are located.
 
     Returns:
         A dictionary of how many annotations there are for each class
@@ -16,14 +21,14 @@ def txt_folders_class_count(input_path):
 
     class_annotation_numbers = defaultdict(int)
 
-    for class_folder in os.listdir(input_path):
-        class_folder_path = os.path.join(input_path, class_folder)
+    for class_folder in os.listdir(path):
+        class_folder_path = os.path.join(path, class_folder)
 
         if os.path.isdir(class_folder_path):
             for file in os.listdir(class_folder_path):
                 if file.endswith('.txt'):
                     annotation_file_path = os.path.join(class_folder_path, file)
-                    with open(annotation_file_path, 'r') as read_file:
+                    with open(annotation_file_path, 'r', encoding='utf-8') as read_file:
                         labels = read_file.readlines()
                         for annotation in labels:
                             class_id = annotation.strip().split()[0]
@@ -34,6 +39,7 @@ def txt_folders_class_count(input_path):
 
     total_annotation = sum(class_annotation_numbers.values())
     print(f"Total number of annotations: {total_annotation}")
+
 
 parser = argparse.ArgumentParser(description='Txt folders class count')
 parser.add_argument('--input_path', type=str, help='Txt folders path', required=True)
